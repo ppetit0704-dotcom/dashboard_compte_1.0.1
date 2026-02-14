ec639ca5291c6b69a9bee9062f21179da83b8c9c
ppetit0704-dotcom <ppetit0704@gmail.com>
2026-02-14 08:07:14 +0100

    Add badgeGreen and badgeOrange functions

--- ui/cards.py
+++ ui/cards.py
@@ -10,7 +10,43 @@ def badge(label, value, color="#FB9223"):
             text-align:center;
             font-size:16px;
             font-weight:600;">
-            <div style="font-size:19px; color:white;">{label}</div>
+            <div style="font-size:14px; color:white;">{label}</div>
+            <div>{value}</div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    
+def badgeGreen(label, value, color="green"):
+    st.markdown(
+        f"""
+        <div style="
+            background-color:{color};
+            padding:12px;
+            border-radius:8px;
+            text-align:center;
+            font-size:16px;
+            font-weight:600;">
+            <div style="font-size:14px; color:white;">{label}</div>
+            <div>{value}</div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    
+def badgeOrange(label, value, color="DarkOrange"):
+    st.markdown(
+        f"""
+        <div style="
+            background-color:{color};
+            padding:12px;
+            border-radius:8px;
+            text-align:center;
+            font-size:16px;
+            font-weight:600;">
+            <div style="font-size:14px; color:white;">{label}</div>
             <div>{value}</div>
         </div>
         """,
